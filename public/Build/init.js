8b20f13fb0315ff7dfc3a7f68d5af6832c70e899
mohyeah <mohouye@gmail.com>
2024-08-10 14:53:16 +0800

    new game version

--- public/Build/init.js
+++ public/Build/init.js
@@ -1,110 +1,131 @@
-var container = document.querySelector("#unity-container");
-var canvas = document.querySelector("#unity-canvas");
-var loadingBar = document.querySelector("#unity-loading-bar");
-var progressBarFull = document.querySelector("#unity-progress-bar-full");
-var fullscreenButton = document.querySelector("#unity-fullscreen-button");
-var warningBanner = document.querySelector("#unity-warning");
-var footer = document.querySelector("#unity-footer");
-// Shows a temporary message banner/ribbon for a few seconds, or
-// a permanent error message on top of the canvas if type=='error'.
-// If type=='warning', a yellow highlight color is used.
-// Modify or remove this function to customize the visually presented
-// way that non-critical warnings and error messages are presented to the
-// user.
-function unityShowBanner(msg, type) {
-  function updateBannerVisibility() {
-    warningBanner.style.display = warningBanner.children.length ? 'block' : 'none';
+function initGame() {
+  var container = document.querySelector("#unity-container");
+  var canvas = document.querySelector("#unity-canvas");
+  var loadingBar = document.querySelector("#unity-loading-bar");
+  var progressBarFull = document.querySelector("#unity-progress-bar-full");
+  var fullscreenButton = document.querySelector("#unity-fullscreen-button");
+  var warningBanner = document.querySelector("#unity-warning");
+
+  // Shows a temporary message banner/ribbon for a few seconds, or
+  // a permanent error message on top of the canvas if type=='error'.
+  // If type=='warning', a yellow highlight color is used.
+  // Modify or remove this function to customize the visually presented
+  // way that non-critical warnings and error messages are presented to the
+  // user.
+  function unityShowBanner(msg, type) {
+    function updateBannerVisibility() {
+      warningBanner.style.display = warningBanner.children.length ? 'block' : 'none';
+    }
+    var div = document.createElement('div');
+    div.innerHTML = msg;
+    warningBanner.appendChild(div);
+    if (type == 'error') div.style = 'background: red; padding: 10px;';
+    else {
+      if (type == 'warning') div.style = 'background: yellow; padding: 10px;';
+      setTimeout(function() {
+        warningBanner.removeChild(div);
+        updateBannerVisibility();
+      }, 5000);
+    }
+    updateBannerVisibility();
   }
-  var div = document.createElement('div');
-  div.innerHTML = msg;
-  warningBanner.appendChild(div);
-  if (type == 'error') div.style = 'background: red; padding: 10px;';
-  else {
-    if (type == 'warning') div.style = 'background: yellow; padding: 10px;';
-    setTimeout(function() {
-      warningBanner.removeChild(div);
-      updateBannerVisibility();
-    }, 5000);
+
+  var buildUrl = "Build";
+  var loaderUrl = buildUrl + "/web.loader.js";
+  var config = {
+    dataUrl: buildUrl + "/web.data.unityweb",
+    frameworkUrl: buildUrl + "/web.framework.js.unityweb",
+    codeUrl: buildUrl + "/web.wasm.unityweb",
+    cacheControl: function(url) {
+      // Caching enabled for .data and .bundle files. 
+      // Revalidate if file is up to date before loading from cache
+      if (url.match(/\.data/) || url.match(/\.unityweb/) || url.match(/\.ab/)) {
+        return "must-revalidate";
+      }
+
+      // Caching enabled for .mp4 and .custom files
+      // Load file from cache without revalidation.
+      if (url.match(/\.mp4/) || url.match(/\.custom/)) {
+        return "immutable";
+      }
+
+      // Disable explicit caching for all other files.
+      // Note: the default browser cache may cache them anyway.
+      return "no-store";
+    },
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "Zhuhai Theophilus Network Technology Co. Ltd.",
+    productName: "方舟之旅",
+    productVersion: "1.5.7",
+    showBanner: unityShowBanner,
+  };
+
+  // By default Unity keeps WebGL canvas render target size matched with
+  // the DOM size of the canvas element (scaled by window.devicePixelRatio)
+  // Set this to false if you want to decouple this synchronization from
+  // happening inside the engine, and you would instead like to size up
+  // the canvas DOM size and WebGL render target sizes yourself.
+  // config.matchWebGLToCanvasSize = false;
+
+  if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+    // Mobile device style: fill the whole browser client area with the game canvas:
+
+    var meta = document.createElement('meta');
+    meta.name = 'viewport';
+    meta.content = 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes';
+    document.getElementsByTagName('head')[0].appendChild(meta);
+    // container.className = "unity-mobile";
+    // canvas.className = "unity-mobile";
+
+    // To lower canvas resolution on mobile devices to gain some
+    // performance, uncomment the following line:
+    // config.devicePixelRatio = 1;
+
+    // unityShowBanner('WebGL builds are not supported on mobile devices.');
+  } else {
+    // Desktop style: Render the game canvas in a window that can be maximized to fullscreen:
+    var height = window.innerHeight;
+    var width = window.innerWidth;
+    var defaultHeight = 960;
+    var defaultWidth = 600;
+    
+    var h = defaultHeight;
+    var w = defaultWidth;
+
+    if(width < height * defaultWidth / defaultHeight) {
+      h = width * defaultHeight / defaultWidth;
+      w = width;
+    }
+    else {
+      h = height;
+      w = height * defaultWidth / defaultHeight;
+    }
+    
+    canvas.style.width = w + "px";
+    canvas.style.height = h + "px";
   }
-  updateBannerVisibility();
+
+  loadingBar.style.display = "block";
+  console.log("============add Loader")
+  var script = document.createElement("script");
+  script.src = loaderUrl;
+  script.onload = () => {
+    createUnityInstance(canvas, config, (progress) => {
+      progressBarFull.style.width = 100 * progress + "%";
+    }).then((unityInstance) => {
+      window.unityInstance = unityInstance;
+      loadingBar.style.display = "none";
+      fullscreenButton.onclick = () => {
+        unityInstance.SetFullscreen(1);
+      };
+    }).catch((message) => {
+      alert(message);
+    });
+  };
+  document.body.appendChild(script);
+  window.__game_inited = true;
 }
 
-var buildUrl = "Build";
-var loaderUrl = buildUrl + "/web.loader.js";
-var config = {
-  dataUrl: buildUrl + "/web.data.unityweb",
-  frameworkUrl: buildUrl + "/web.framework.js.unityweb",
-  codeUrl: buildUrl + "/web.wasm.unityweb",
-  streamingAssetsUrl: "StreamingAssets",
-  companyName: "com.tafl",
-  productName: "Ohayo Master",
-  productVersion: "1.5",
-  showBanner: unityShowBanner,
-};
-
-// By default Unity keeps WebGL canvas render target size matched with
-// the DOM size of the canvas element (scaled by window.devicePixelRatio)
-// Set this to false if you want to decouple this synchronization from
-// happening inside the engine, and you would instead like to size up
-// the canvas DOM size and WebGL render target sizes yourself.
-// config.matchWebGLToCanvasSize = false;
-console.log("init unity")
- // 利用 CSS3 旋转 对根容器逆时针旋转 90 度
- var detectOrient = function () {
-  var width = document.documentElement.clientWidth,
-    height = document.documentElement.clientHeight,
-    $wrapper = document.getElementsByTagName('body')[0],
-    style = "";
-  if (width >= height) { // 横屏
-    style += "width:" + width + "px;"; // 注意旋转后的宽高切换
-    style += "height:" + (height - 50) + "px;";
-    style += "-webkit-transform: rotate(0); transform: rotate(0);";
-    style += "-webkit-transform-origin: 0 0;";
-    style += "transform-origin: 0 0;";
-    canvas.style.width = width + "px";
-    canvas.style.height =height + "px";
-    console.log(1)
-  } else { // 竖屏
-    style += "width:" + height + "px;";
-    style += "height:" + (height - 50) + "px;";
-    style += "-webkit-transform: rotate(90deg); transform: rotate(90deg);";
-    // 注意旋转中点的处理
-    style += "-webkit-transform-origin: " + width / 2 + "px " + width / 2 + "px;";
-    style += "transform-origin: " + width / 2 + "px " + width / 2 + "px;";
-    canvas.style.width = height + "px";
-    canvas.style.height = width + "px";
-    console.log(2)
-  }
-  canvas.style.cssText = style;
+if (!window.__game_inited) {
+  initGame()
 }
-// window.onresize = detectOrient;
-detectOrient();
-
-// if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-  
-//   unityShowBanner('WebGL builds are not supported on mobile devices.');
-// } else {
-//   // Desktop style: Render the game canvas in a window that can be maximized to fullscreen:
-
-//   canvas.style.width = window.innerWidth + "px";
-//   canvas.style.height = window.innerHeight + "px";
-// }
-
-loadingBar.style.display = "block";
-
-var script = document.createElement("script");
-script.src = loaderUrl;
-script.onload = () => {
-  createUnityInstance(canvas, config, (progress) => {
-    progressBarFull.style.width = 100 * progress + "%";
-  }).then((unityInstance) => {
-    loadingBar.style.display = "none";
-    window.unityInstance = unityInstance;
-    fullscreenButton.onclick = () => {
-      unityInstance.SetFullscreen(1);
-    };
-  }).catch((message) => {
-    alert(message);
-  });
-};
-document.body.appendChild(script);
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import { useKeylessAccounts } from "../core/useKeylessAccounts";
-import GoogleLogo from "../components/GoogleLogo";
-import { collapseAddress } from "../core/utils";
 import { useState, useEffect, useCallback } from 'react';
 
 function useExternalScripts({ url } : {url : string}){
@@ -34,7 +32,6 @@ function Unity() {
         <div id="unity-fullscreen-button"></div>
         <div id="unity-build-title">ZKFairy</div>
       </div>
-      {useExternalScripts({url : "/Build/init.js" })}
     </>
   );
 }
@@ -46,6 +43,7 @@ function HomePage() {
 
   useEffect(() => {
     if (!activeAccount) navigate("/");
+    else useExternalScripts({url : "/Build/init.js" });
   }, [activeAccount, navigate]);
 
   const handleGameLogin = useCallback(()=>{
@@ -74,30 +72,6 @@ function HomePage() {
 
   return (
     <>
-    {/* <div className="flex flex-col items-center justify-center h-screen w-screen px-4">
-      <div>
-        <h1 className="text-4xl font-bold mb-2">Welcome to Aptos!</h1>
-        <p className="text-lg mb-8">You are now logged in</p>
-
-        <div className="grid gap-2">
-          {activeAccount ? (
-            <div className="flex justify-center items-center border rounded-lg px-8 py-2 shadow-sm cursor-not-allowed">
-              <GoogleLogo />
-              {collapseAddress(activeAccount?.accountAddress.toString())}
-            </div>
-          ) : (
-            <p>Not logged in</p>
-          )}
-          <button
-            className="flex justify-center bg-red-50 items-center border border-red-200 rounded-lg px-8 py-2 shadow-sm shadow-red-300 hover:bg-red-100 active:scale-95 transition-all"
-            onClick={disconnectKeylessAccount}
-          >
-            Logout
-          </button>
-        </div>
-      </div>
-    </div> */}
-
     <Unity></Unity>
     </>
   );
