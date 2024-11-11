bfed13b177c6626325fe07553730ff34b2a69f47
mohyeah <mohouye@gmail.com>
2024-11-11 09:23:03 +0800

    更新

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -2,10 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 
-export const GAME_WASM_PATH = "./Build/f599de14b4276652a6bbbd8695c8a679.wasm.gz";
-export const GAME_LOADER_PATH = "./Build/bb0d9ecdb05db3e84da20bd14a4f84dc.loader.js";
-export const GAME_DATA_PATH  = "./Build/b546b0e910da71dc42834c7ae72de426.data.gz";
-export const GAME_FRAMEWORK_PATH = "./Build/008b4fe435330425b80028923a1813a3.framework.js.gz";
+export const GAME_WASM_PATH = "./Build/4cc1fd1b0376f0ea14ac05c2cc714510.wasm.unityweb";
+export const GAME_LOADER_PATH = "./Build/5336a4b2c43054286fd70b1faa467eee.loader.js";
+export const GAME_DATA_PATH  = "./Build/4a095e359b47c33d5e54b0dd294f32bf.data.unityweb";
+export const GAME_FRAMEWORK_PATH = "./Build/008b4fe435330425b80028923a1813a3.framework.js.unityweb";
 export const APPID = "ShATk8B1VYKHrzx3";
 
 export const postJson = async (url : string, param: any) => {
