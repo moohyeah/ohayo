db7cbcb8105ed449868f7dab45c0ef6337f46b37
mohyeah <mohouye@gmail.com>
2024-11-22 10:27:08 +0800

    更新

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -4,7 +4,7 @@
 
 export const GAME_WASM_PATH = "./Build/1f29da633c726fc349da7b8490b7cdff.wasm.unityweb";
 export const GAME_LOADER_PATH = "./Build/5336a4b2c43054286fd70b1faa467eee.loader.js";
-export const GAME_DATA_PATH  = "./Build/55a91b656287efd719a244b53b934d59.data.unityweb";
+export const GAME_DATA_PATH  = "./Build/e729521de9d01cd5bfa599951fb18e72.data.unityweb";
 export const GAME_FRAMEWORK_PATH = "./Build/a25f8f5b2e5ac008fe39bad378d748ab.framework.js.unityweb";
 export const APPID = "ShATk8B1VYKHrzx3";
 
