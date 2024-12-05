644ddd69663997ba91d8b9931a597bcbf0423f44
mohyeah <mohouye@gmail.com>
2024-12-05 17:10:17 +0800

    更新

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -4,17 +4,17 @@
 
 export const GAME_WASM_PATH = "./Build/4615d69aceee890b23c9e922b140b396.wasm.unityweb";
 export const GAME_LOADER_PATH = "./Build/5336a4b2c43054286fd70b1faa467eee.loader.js";
-export const GAME_DATA_PATH  = "./Build/f15d7be4d6637772d2b4960970f0585e.data.unityweb";
+export const GAME_DATA_PATH  = "./Build/68f1fbeb5a5047be82a32f2526e80784.data.unityweb";
 export const GAME_FRAMEWORK_PATH = "./Build/7567b985417689c0d50a5f7dac1b2604.framework.js.unityweb";
 
 export const postJson = async (url : string, param: any) => {
-    const result = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(param),
-      });
-    const data = await result.json();
-    return data
+  const result = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(param),
+    });
+  const data = await result.json();
+  return data
 }
