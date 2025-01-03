5feece9a4f4c0575ce9c7eda617aeab12259e686
mohyeah <mohouye@gmail.com>
2025-01-03 11:23:40 +0800

    实时奖池 修复bug

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -2,10 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 
-export const GAME_WASM_PATH = "./Build/0a47ac674e6fde4308098eab89f20090.wasm.unityweb";
+export const GAME_WASM_PATH = "./Build/0df2ce24b4a458fac315ab4e51d36287.wasm.unityweb";
 export const GAME_LOADER_PATH = "./Build/5336a4b2c43054286fd70b1faa467eee.loader.js";
-export const GAME_DATA_PATH  = "./Build/68e2c23a9eadc8a3143812c7702700a4.data.unityweb";
-export const GAME_FRAMEWORK_PATH = "./Build/a914b16d4513ae8763e067595893f8c4.framework.js.unityweb";
+export const GAME_DATA_PATH  = "./Build/f20a8689b036ae25257bcb3b897af996.data.unityweb";
+export const GAME_FRAMEWORK_PATH = "./Build/a23d8af17950c310873985e1a1627e58.framework.js.unityweb";
 
 export const postJson = async (url : string, param: any) => {
   const result = await fetch(url, {
